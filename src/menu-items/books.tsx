8b4b01e68a334bef5a27787fe6ff168091099387
johan253 <johannjo2000@gmail.com>
2025-05-27 14:25:04 -0700

    Added 'List Books' to Navbar

---
+++ src/menu-items/books.tsx
@@ -0,0 +1,37 @@
+// third-party
+import { FormattedMessage } from 'react-intl';
+
+// assets
+import { BookRounded, Add } from '@mui/icons-material';
+
+// type
+import { NavItemType } from 'types/menu';
+
+// icons
+const icons = { BookRounded, PlusOne: Add };
+
+// ==============================|| MENU ITEMS - PAGES ||============================== //
+
+const pages: NavItemType = {
+  id: 'group-books',
+  title: <FormattedMessage id="books" />,
+  type: 'group',
+  children: [
+    {
+      id: 'list-books',
+      title: <FormattedMessage id="list-books" />,
+      type: 'item',
+      url: '/books',
+      icon: icons.BookRounded
+    }
+    // {
+    //   id: 'create-book',
+    //   title: <FormattedMessage id="create-book" />,
+    //   type: 'item',
+    //   url: '/books/create',
+    //   icon: icons.PlusOne
+    // }
+  ]
+};
+
+export default pages;
